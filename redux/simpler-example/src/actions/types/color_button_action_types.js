b94319dfdf7f72c277e2dd9362350476a15104aa
kamikazePT <afonso.1995@hotmail.com>
2018-06-06 18:26:35 +0100

    #redux ~updated
    Signed-off-by: kamikazePT <afonso.1995@hotmail.com>

--- redux/simpler-example/src/actions/types/color_button_action_types.js
+++ redux/simpler-example/src/actions/types/color_button_action_types.js
@@ -1,7 +1,5 @@
 import { objectUtils } from 'bsimprimir-core-utils';
 
 export default objectUtils.arrayToObject([
-  'DO_SELECT_COLOR',
-  'DO_FETCH_COLORS_START',
-  'DO_FETCH_COLORS_SUCCESS'
+  'DO_SELECT_COLOR'
 ]);
